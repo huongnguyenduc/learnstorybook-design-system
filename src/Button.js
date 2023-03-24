53c96a90f0939ab0405907badb8a13ee9f8fea29
huongnguyenduc <duchuong007@gmail.com>
2023-03-24 08:54:45 +0700

    style: fix button

--- src/Button.js
+++ src/Button.js
@@ -33,9 +33,7 @@ const SIZES = {
 };
 
 const StyledButton = styled.button`
-  border: 10px solid red;
-  font-size: 20px;
-  //border: 0;
+  border: 0;
   border-radius: 3em;
   cursor: pointer;
   display: inline-block;
@@ -54,7 +52,7 @@ const StyledButton = styled.button`
   background: transparent;
 
 
-    //font-size: ${props => (props.size === SIZES.SMALL ? typography.size.s1 : typography.size.s2)}px;
+  font-size: ${props => (props.size === SIZES.SMALL ? typography.size.s1 : typography.size.s2)}px;
   font-weight: ${typography.weight.extrabold};
   line-height: 1;
 
